244ada8864672a6e04cec58cb865cc5eddeb8485
Fabricio <fabricio.l.v.magalhaes@gmail.com>
2019-03-04 13:47:29 -0300

    Excluindo lançamentos e o decorador @ViewChild

--- src/app/pessoas/pessoa.service.ts
+++ src/app/pessoas/pessoa.service.ts
@@ -44,4 +44,18 @@ export class PessoaService {
       )
   }
 
+  pesquisarTodas(): Promise<any> {
+    const headers = new Headers();
+    headers.append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    return this.http.get(`${this.pessoaUrl}`, {headers})
+      .toPromise()
+      .then(
+        response => {
+          const resultado =  response.json();
+          return resultado.content;
+        }
+      )
+  }
+
 }
